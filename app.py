74adb449dbe2eb3b01715596c404e68e53d0812c
Rahul-3043 <3055rahulgitte@gmail.com>
2025-11-02 23:09:27 +0530

    Rename home function to hello and update message

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello from Rahul's CI/CD App 🚀"
+def hello():
+    return "🚀 Hello from Flask running in Docker on EC2!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
