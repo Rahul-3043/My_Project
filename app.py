34ce167e78fd41e29de66f311f86eff3fc55d742
Rahul-3043 <3055rahulgitte@gmail.com>
2025-11-07 21:45:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+# app.py
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "🚀 Hello from Flask running in Docker on EC2!"
+    return "Hello from Rahul's CI/CD worjing properly 🚀"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
